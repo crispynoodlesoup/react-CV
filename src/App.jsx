dbaba82de6a7c45c07b1dd29184681770cb12bc9
crispynoodlesoup <thepanman321@gmail.com>
2023-09-17 18:34:59 -0500

    create a basic app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import "../style/app.css";
+
+function Accordion() {
+  return (
+    <section>
+      <div className="accordion-switch">hide content</div>
+      <div className="accordion-content">content</div>
+    </section>
+  );
+}
+
+function CV() {
+  return (
+    <form className="application">
+      <Accordion></Accordion>
+      <Accordion></Accordion>
+      <Accordion></Accordion>
+    </form>
+  );
+}
+
 function App() {
   return (
     <>
-      <p>hello</p>
+      <CV></CV>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
