9df68251e564a4a693f62c80aaeb5374e178bd57
crispynoodlesoup <thepanman321@gmail.com>
2023-09-18 19:48:47 -0500

    style the form

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,42 @@ import { useState } from "react";
 import "../style/app.css";
 
 function General() {
-  return <div>General</div>;
+  return <div className="accordion-content">General</div>;
 }
 
 function Educational() {
-  return <div>Educational</div>;
+  return <div className="accordion-content">Educational</div>;
 }
 
 function Practical() {
-  return <div>Practical</div>;
+  return <div className="accordion-content">Practical</div>;
 }
 
 function Accordion({ children, accordionId, selected, onSelect }) {
   return (
-    <section>
-      <div className="accordion-switch" onClick={() => {onSelect(accordionId)}}>{selected === accordionId ? "Editing" : "Edit" }</div>
+    <section className="accordion">
+      <div className="accordion-switch" onClick={() => {onSelect(accordionId)}}>{accordionId}</div>
       {selected === accordionId ? children : null}
     </section>
   );
 }
 
 function CV() {
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState("General");
 
   function select(accordionId) {
     setSelected(accordionId);
   }
 
   return (
-    <form className="application">
-      <Accordion accordionId={0} selected={selected} onSelect={select}>
+    <form className="cv-form">
+      <Accordion accordionId={"General"} selected={selected} onSelect={select}>
         <General></General>
       </Accordion>
-      <Accordion accordionId={1} selected={selected} onSelect={select}>
+      <Accordion accordionId={"Educational"} selected={selected} onSelect={select}>
         <Educational></Educational>
       </Accordion>
-      <Accordion accordionId={2} selected={selected} onSelect={select}>
+      <Accordion accordionId={"Practical"} selected={selected} onSelect={select}>
         <Practical></Practical>
       </Accordion>
       <button type="button">Submit</button>
